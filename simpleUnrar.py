3272954741de496906fe42c6d89a6553cd401bcc
Tenyage <59667932+Tenyage@users.noreply.github.com>
2020-06-03 13:13:22 -0400

    Update and rename unrar.py to simpleUnrar.py

--- simpleUnrar.py
+++ simpleUnrar.py
@@ -1,17 +1,29 @@
 from unrar import rarfile
 import sys
 
-rar = rarfile.RarFile('unrar.rar')
+in_path = input('Enter the input file path:')
+try:
+	rar = rarfile.RarFile(in_path)
+except:
+	print('input file not found!', sys.exc_info()[0])
+	sys.exit()
 rar.namelist()
 rar.printdir()
-opened = False
-with open('dictionary.txt') as my_file:
-    lines = my_file.readlines()
 
+dic_path = input('Enter the dictionary file path:')
+try:
+	with open(dic_path) as my_file:
+		lines = my_file.readlines()
+except:
+	print('dictionary file not found!', sys.exc_info()[0])
+	sys.exit()
+
+out_path = input('Enter the output file path (Current directory if none):')
+opened = False
 for line in lines:
     pwd = str(line.strip('\n'))
     try:
-        rar.extractall(pwd=pwd)
+        rar.extractall(path=out_path, pwd=pwd)
         print("real password is : %s" %(pwd))
         #add break if want to extract file
         opened = True
